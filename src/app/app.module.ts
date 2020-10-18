011d4036f66c61e16bbc9017988523a25f792a72
Hasan Ahmed <ahmedhasan@gmail.com>
2020-10-18 12:20:10 -0700

    star rater

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TypeAheadComponent } from './my-components/type-ahead/type-ahead.compon
 import { MyComponentListsComponent } from './entry-components/my-component-lists/my-component-lists.component';
 import { AngularDefaultPageComponent } from './entry-components/angular-default-page/angular-default-page.component';
 import { TypeAheadExamplePageComponent } from './page-components/type-ahead-example-page/type-ahead-example-page.component';
+import { StarRaterComponent } from './my-components/star-rater/star-rater.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TypeAheadExamplePageComponent } from './page-components/type-ahead-exam
     TypeAheadComponent,
     MyComponentListsComponent,
     AngularDefaultPageComponent,
-    TypeAheadExamplePageComponent
+    TypeAheadExamplePageComponent,
+    StarRaterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-components/star-rater/star-rater.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-star-rater',
+  templateUrl: './star-rater.component.html',
+  styleUrls: ['./star-rater.component.scss']
+})
+export class StarRaterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
