8ebe25480b4e0bed23acd5fc9dda869839563e4d
Hasan Ahmed <ahmedhasan@gmail.com>
2020-10-18 16:17:59 -0700

    star rater wip

--- src/app/my-components/star-rater/star-rater.component.ts
+++ src/app/my-components/star-rater/star-rater.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, SimpleChanges } from '@angular/core';
+import { EventEmitter } from 'protractor';
+import { enableDebugTools } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-star-rater',
+  selector: 'star-rater',
   templateUrl: './star-rater.component.html',
   styleUrls: ['./star-rater.component.scss']
 })
-export class StarRaterComponent implements OnInit {
+export class StarRaterComponent implements OnChanges {
+
+  @Input() numberOfStars = 5;
+  @Input() rating = -1;
+  @Output() onRatingChange;
+
+  public readonly starChar = "&#9733";
+  public starStates: starState[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initStars();
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.numberOfStars) {
+      this.initStars();
+    }
+  }
+
+  public onContainerMouseLeave() {
+    this.highlight(this.rating);
+    console.log("onMouseoutFromContainer");
+  }
+
+  public onStarMouseEnter(index: number) {
+    this.highlight(index);
+    console.log("onMouseoverStar");
+  }
+
+  public onStarClick(index: number) {
+    this.rating = this.rating == index ? index - 1 : index;
+    this.highlight(this.rating);
+  }
+  
+  private highlight(index: number) {
+    for (let i = 0; i < this.numberOfStars; i++) {
+      this.starStates[i].enabled = (i <= index);
+    }
+  }
+
+  private initStars() {
+    this.starStates = [];
+    for (let i = 0; i < this.numberOfStars; i++) {
+      this.starStates.push({
+        id: i,
+        enabled: false
+      });
+    }
+  }
+}
+
+interface starState {
+  id: number,
+  enabled: boolean
 }
